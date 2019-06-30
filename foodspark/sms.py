dbab5e080fe185adb11db78aa39ce3341e72e283
wan862 <wan862@gmail.com>
2019-07-01 01:22:38 +0800

    hide twilio sid and token

--- foodspark/sms.py
+++ foodspark/sms.py
@@ -1,9 +1,10 @@
+import os
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "ACecd566249532a05854e0b664697499db"
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
 # Your Auth Token from twilio.com/console
-auth_token  = "fa4757ca599b15790a4ed758009b18d7"
+auth_token  = os.getenv("TWILIO_AUTH_TOKEN")
 # Your twilio number
 from_number = "+17204632175"
 
