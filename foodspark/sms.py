68471932a1654af5efea049dd8674b5837a42e27
wan862 <wan862@gmail.com>
2019-07-01 00:54:16 +0800

    changed token

--- foodspark/sms.py
+++ foodspark/sms.py
@@ -3,7 +3,7 @@ from twilio.rest import Client
 # Your Account SID from twilio.com/console
 account_sid = "ACecd566249532a05854e0b664697499db"
 # Your Auth Token from twilio.com/console
-auth_token  = "38baf405438269cda879f2e173750094"
+auth_token  = "fa4757ca599b15790a4ed758009b18d7"
 # Your twilio number
 from_number = "+17204632175"
 
